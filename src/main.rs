ca387fd23af5db97d92b70dd81b626ae34788d9e
NoSpawnn <78027161+NoSpawnn@users.noreply.github.com>
2025-05-13 15:51:13 +0100

    random test

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,7 @@ mod grid;
 use grid::Grid;
 
 fn main() -> Result<(), grid::Error> {
-    let mut grid = Grid::new_empty();
-    grid.set_cell(8, 8, 9)?;
-    grid.set_cell(3, 4, 1)?;
+    let mut grid = Grid::new_random();
     println!("{}", grid);
 
     Ok(())
