d847f02a3b9cfbfa393cbba4ceab4fb753f6075e
NoSpawnn <adam.blighe@protonmail.com>
2025-05-19 01:36:54 +0100

    Set `WGPU_POWER_PREF=high` envvar for.. linux/flatpak reasons

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,14 @@
+use std::env;
+
 use gui::gui::Program;
 
 mod gui;
 mod sudoku;
 
 fn main() -> iced::Result {
+    unsafe {
+        env::set_var("WGPU_POWER_PREF", "high");
+    }
+
     Program::run()
 }
